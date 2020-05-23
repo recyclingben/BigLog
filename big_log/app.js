ed334c5b0c41f90309144819dd10e21cd4eb1bfa
recyclingben <bdcubbage@gmail.com>
2020-05-23 16:14:23 -0400

    Initial commit.

---
+++ big_log/app.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", require("express-handlebars")());
+app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.redirect("/home"));
+app.get("/home", (req, res) => res.render("home"));
+app.get("/school", (req, res) => res.render("school"));
+app.listen(port);
