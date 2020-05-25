a13a9ba342e649662f1e1df4f779d50f7bd1b022
recyclingben <bdcubbage@gmail.com>
2020-05-25 15:38:00 -0400

    Pass and use titles.

--- big_log/app.js
+++ big_log/app.js
@@ -9,10 +9,10 @@ app.set('views', path.join(__dirname, "public"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.redirect("/home"));
-app.get("/home", (req, res)       => res.render("home"));
-app.get("/welcome", (req, res)    => res.render("welcome"));
-app.get("/school", (req, res)     => res.render("school"));
-app.get("/poem", (req, res)       => res.render("poem"));
-app.get("/evil", (req, res)       => res.render("evil"));
-app.get("/branchless", (req, res) => res.render("branchless"));
+app.get("/home", (req, res)       => res.render("home",       { title: "Home" }));
+app.get("/welcome", (req, res)    => res.render("welcome",    { title: "I Have a Blog Now" }));
+app.get("/school", (req, res)     => res.render("school",     { title: "Why I Don't Like School and What's Wrong About It" }));
+app.get("/poem", (req, res)       => res.render("poem",       { title: "CSS" }));
+app.get("/evil", (req, res)       => res.render("evil",       { title: "Please Don't Do This" }));
+app.get("/branchless", (req, res) => res.render("branchless", { title: "Adventures in Branchless Computing" }));
 app.listen(port);
