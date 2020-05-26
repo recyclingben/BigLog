dc4d4024346018a40ad325d1958af74277105355
recyclingben <bdcubbage@gmail.com>
2020-05-26 13:40:11 -0400

    Move handlebars files out of public directory.

--- big_log/app.js
+++ big_log/app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.engine("handlebars", require("express-handlebars")());
 app.set("view engine", "handlebars");
-app.set('views', path.join(__dirname, "public"));
+app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.redirect("/home"));
